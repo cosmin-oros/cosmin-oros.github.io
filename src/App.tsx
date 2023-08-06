b2b719d087a29992401fc9e9e5640dfb4015fead
Cosmin <oroscosmin09@gmail.com>
2023-08-06 17:57:34 +0300

    components init

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
+import { BrowserRouter } from "react-router-dom";
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
+
 const App = () => {
 
   return (
-    <>
-      Portfolio
-    </>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-color bg-no-repeat bg-center">
+          <Navbar/>
+          <Hero/>
+          <About/>
+          <Experience/>
+          <Tech/>
+          <Works/>
+          <Feedbacks/>
+          <div className="relative z-0">
+            <Contact/>
+            <StarsCanvas/>
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
